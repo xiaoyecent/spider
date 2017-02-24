4acb12aa9d72d9bd99ef96c8da4a1a9e5d11c096
xiaoye_pypy <biqqanquan@163.com>
2017-02-24 21:13:56 +0800

    anquanke_spider

---
+++ anquanke_spider.py
@@ -0,0 +1,30 @@
+'''
+@author: xiaoye
+'''
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def doget(url, headers=''):
+    try:
+        r = requests.get(url, headers=headers, timeout=6)
+        if r.status_code == 200:
+            cont = r.content
+        soup = BeautifulSoup(cont, 'lxml', from_encoding='utf-8')
+        bqs = soup.find_all('li', class_='clearfix')
+        #print bqs
+        for bq in bqs:
+            #print bq.get_text()
+            b = bq.find('a', class_='title')
+            print b.get_text()
+            a = bq.find('p', class_='desc')
+            print a.get_text().replace(u'\u200b', u' ') + '\n'
+    except:
+        pass
+    
+if __name__ == "__main__":
+    for i in range(1,5):
+        ii = '%d' % i
+        url = "http://bobao.360.cn/news/&page=" + ii
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+        doget(url, headers=headers)
